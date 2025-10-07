804391e71255b3a3954946e2f1e9fed9cb08e809
Alberto Fanton <alberto.fanton@protonmail.com>
2025-10-07 21:44:57 +0200

    build: add Nix flake for reproducible development environment

---
+++ flake.nix
@@ -0,0 +1,41 @@
+{
+  description = "Relage project powered by flake-parts";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs = inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+
+      perSystem = { config, self', inputs', pkgs, system, ... }: {
+        packages = {
+          hello = pkgs.hello;
+          default = config.packages.hello;
+        };
+
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            hello
+            ollama
+          ];
+          
+          shellHook = ''
+            echo "Welcome to the Relage development environment!"
+            echo "Available packages: hello"
+          '';
+        };
+      };
+
+      flake = {
+        # Add any flake-level configurations here
+      };
+    };
+}
