74a0e1a3c79a9ad6403a8e4cc47892569acf6606
Alberto Fanton <alberto.fanton@protonmail.com>
2025-10-19 19:44:54 +0200

    refactor: apply hint and restructure test

--- hs-spanshot/src/Collect.hs
+++ hs-spanshot/src/Collect.hs
@@ -1,7 +1,6 @@
 module Collect (
     collectFromFile,
     collectFromStream,
-    CollectOptions (..),
 ) where
 
 import Control.Concurrent (threadDelay)
@@ -104,13 +103,13 @@ let linesStream = Q.lines someByteStream
 events <- S.toList_ $ collectFromStream "myfile.log" linesStream
 @
 -}
-collectFromStream :: Text -> Stream (Q.ByteStream IO) IO () -> Stream (Of CollectEvent) IO ()
+collectFromStream :: Text -> Stream (Q.ByteStream IO) IO a -> Stream (Of CollectEvent) IO a
 collectFromStream sourcePath linesStream = do
     -- Convert Stream (ByteStream m) to Stream (Of ByteString) using mapped
     let strictLines = S.mapped Q.toStrict linesStream
     -- Now we can zip with indices
     S.mapM (uncurry $ lineToEvent sourcePath) $
-        S.zip (S.each [0 ..]) strictLines
+        S.zip (S.iterate succ 0) strictLines
 
 {- | Convert a single line (as ByteString) into a 'CollectEvent'.
 
